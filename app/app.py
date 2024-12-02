3b36a42be3e5fc5d6d2c0a06e81a51a3b7d9a19b
penoFahmi <penofahmi@gmail.com>
2024-12-02 10:22:40 +0700

    alhamdulillah

--- app/app.py
+++ app/app.py
@@ -3,13 +3,10 @@ import joblib
 import numpy as np
 import os
 
+# Load model
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-model_path = os.path.join(BASE_DIR, "../models/best_stroke_model.pkl")
+model_path = os.path.join(BASE_DIR, "../models/stroke_best_model.pkl")
 model = joblib.load(model_path)
-
-
-# # Load model
-# model = joblib.load('../models/stroke_model.pkl')
 
 # Set up title and page layout
 st.set_page_config(page_title="Aplikasi Prediksi Stroke", layout="wide")
@@ -17,7 +14,7 @@ st.title("Aplikasi Prediksi Stroke")
 
 # Deskripsi aplikasi
 st.markdown("""
-Aplikasi ini memprediksi apakah seseorang berisiko mengalami stroke berdasarkan data usia, BMI, level glukosa, jenis kelamin, dan status merokok.
+Aplikasi ini memprediksi apakah seseorang berisiko mengalami stroke berdasarkan data usia, BMI, level glukosa, jenis kelamin, status merokok, status pernikahan, dan jenis tempat tinggal.
 Silakan masukkan data Anda untuk melihat prediksi.
 """)
 
@@ -28,15 +25,20 @@ glucose = st.number_input("Rata-rata Level Glukosa", min_value=50.0, max_value=3
 
 gender = st.selectbox("Jenis Kelamin", options=["Laki-laki", "Perempuan"])
 smoking_status = st.selectbox("Status Merokok", options=["Tidak pernah merokok", "Pernah merokok", "Merokok"])
+married = st.selectbox("Apakah Anda sudah menikah?", options=["Ya", "Tidak"])
+residence = st.selectbox("Jenis Tempat Tinggal", options=["Urban", "Rural"])
 
 # Encode inputs
 gender_encoded = 1 if gender == "Laki-laki" else 0
 smoking_encoded = {"Tidak pernah merokok": 0, "Pernah merokok": 1, "Merokok": 2}[smoking_status]
+married_encoded = 1 if married == "Ya" else 0
+residence_encoded = 1 if residence == "Urban" else 0
+
+# Gabungkan semua fitur
+input_data = np.array([[age, bmi, glucose, gender_encoded, smoking_encoded, married_encoded, residence_encoded]])
 
 # Predict
 if st.button("Prediksi"):
-    input_data = np.array([[age, bmi, glucose, gender_encoded, smoking_encoded]])
     prediction = model.predict(input_data)
     result = "Stroke" if prediction[0] == 1 else "Tidak Stroke"
     st.write(f"Prediksi hasilnya adalah: **{result}**")
-    
